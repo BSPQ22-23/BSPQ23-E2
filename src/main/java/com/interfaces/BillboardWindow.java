0df798d34a4b2fbf01f4790fe0ec74e555b80e9a
XabiGomez <xabier.gomezu@opendeusto.es>
2023-04-30 20:31:11 +0200

    Pelis

--- src/main/java/com/interfaces/BillboardWindow.java
+++ src/main/java/com/interfaces/BillboardWindow.java
@@ -1,6 +1,13 @@
 package com.interfaces;
 
 import javax.swing.*;
+
+import com.example.pojo.Film;
+import com.example.server.FilmResource;
+
+import dao.FilmDAO;
+
+import java.util.List;
 import java.awt.*;
 import java.awt.event.*;
 
@@ -10,7 +17,7 @@ public class BillboardWindow extends JFrame implements ActionListener {
 	 */
 	private static final long serialVersionUID = 1L;
 	private JButton[] movieButtons;
-	
+	private List<Film> films;
 
     public BillboardWindow() {
         // Configurar la ventana
@@ -26,44 +33,67 @@ public class BillboardWindow extends JFrame implements ActionListener {
         ImageIcon son = new ImageIcon();
         
         setSize(400, 400);
+    
+        films = FilmDAO.getInstance().getAll();
         
-        movieButtons = new JButton[5];
-        movieButtons[0] = new JButton("Spider-Man: No Way Home");
-        movieButtons[0].setIcon(spid);
-        movieButtons[1] = new JButton("The Batman");
-        movieButtons[1].setIcon(bat);
-        movieButtons[2] = new JButton("Avatar 2");
-        movieButtons[2].setIcon(ava);
-        movieButtons[3] = new JButton("Jurassic World: Dominion");
-        movieButtons[3].setIcon(jur);
-        movieButtons[4] = new JButton("Sonic the Hedgehog 2");
-        movieButtons[4].setIcon(son);
+     
+        JPanel binf = new JPanel();
+       
 
-        // Agregar los componentes a la ventana
         Container container = getContentPane();
         container.setLayout(new BorderLayout());
         container.add(titleLabel, BorderLayout.NORTH);
         container.add(moviePanel, BorderLayout.CENTER);
-        for (int i = 0; i < movieButtons.length; i++) {
-            moviePanel.add(movieButtons[i]);
-            movieButtons[i].addActionListener(this);
+        container.add(binf, BorderLayout.SOUTH);
+        
+        for (int i = 0; i < films.size(); i++) {
+            Film film = films.get(i);
+            JButton button = new JButton(film.getName()); 
+            button.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                	 for (int i = 0; i < movieButtons.length; i++) {
+                         if (e.getSource() == movieButtons[i]) {
+                             String movieName = movieButtons[i].getText();
+                             new SesionesWindow(movieName);
+                             break;
+                         }
+                     }
+                }
+            });
+            movieButtons[i] = button; // agrega el botón al arreglo
+            add(button); // agrega el botón al panel
         }
+        
+       
+        JButton back = new JButton("Back");  
+        binf.add(back);
+        	back.addActionListener(new ActionListener() {
+
+    			@Override
+    			public void actionPerformed(ActionEvent e) {
+    				MainWindow vp = new MainWindow();
+    				dispose();
+    				
+    			}
+    		});
 
         // Mostrar la ventana
         setVisible(true);
     }
 
-    public void actionPerformed(ActionEvent e) {
-        for (int i = 0; i < movieButtons.length; i++) {
-            if (e.getSource() == movieButtons[i]) {
-                String movieName = movieButtons[i].getText();
-                new SesionesWindow(movieName);
-                break;
-            }
-        }
-    }
+    
+    
 
     public static void main(String[] args) {
         BillboardWindow bw = new BillboardWindow();
     }
+
+
+
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
 }
