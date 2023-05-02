103200362d9499f77619fc729e02a8c09bc1604c
Iker-Gorostiaga <iker.gorostiaga@opendeusto.es>
2023-05-02 19:29:31 +0200

    pom con problemas mentales

--- tests/tests/TestClientApp.java
+++ tests/tests/TestClientApp.java
@@ -6,13 +6,13 @@ import static org.mockito.Mockito.*;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
 
 public class TestClientApp {
 
 	@Before
 	public void setUp() throws Exception {
 	}
-
 	@After
 	public void tearDown() throws Exception {
 	}
